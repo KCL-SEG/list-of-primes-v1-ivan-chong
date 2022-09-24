cb728001d12d98afebd8bcb2865e51c4fab38c7a
Ka Chun Chong <ka.c.chong@kcl.ac.uk>
2022-09-24 16:27:14 +0100

    Add initial implementation of primes

--- primes.py
+++ primes.py
@@ -3,4 +3,11 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    if number_of_primes <= 0:
+        return
+    while len(list) < number_of_primes:
+        if all(num%i != 0 for i in range(2, num)):
+            list.append(num)
+        num+=1
     return list
